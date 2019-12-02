6ce1af1f2726ef863d86a6fbaf5e048ae54b4d06
Iago Bruno <httpiago@gmail.com>
2019-12-02 18:39:00 -0300

    feat: Adição de links de navegação

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { configs } from '../data'
 import { checkIfBusinessIsOpen } from '../common/functions'
 
@@ -12,29 +12,36 @@ const Header: React.FunctionComponent<Props> = ({ mode = 'compact' }) => {
   const isOpenNow = useMemo(() => checkIfBusinessIsOpen(configs.deliveryTime), [])
 
   return (
-    <header className={`header--${mode}`}>
-      {mode === 'expanded' && <img className="header__cover" src={configs.headerCover} />}
-      <div className="header__alignBottom center">
-        <div className="col">
-          <Link to="/">
-            <h1 className="header__title">{configs.title}</h1>
-          </Link>
-          <p className="header__infos">
-            <span className={`header__infos__status ${isOpenNow ? 'opened' : 'closed'}`}>
-              {isOpenNow ? 'Aberto!' : 'Fechado'}
-            </span>{' • '}
-            {configs.randomPlace}{' • '}
-            Entrega {configs.averageDeliveryDuration}
-          </p>
+    <>
+      <header className={`header--${mode}`}>
+        {mode === 'expanded' && <img className="header__cover" src={configs.headerCover} />}
+        <div className="header__alignBottom center">
+          <div className="col">
+            <h1 className="header__title"><Link to="/">{configs.title}</Link></h1>
+            <p className="header__infos">
+              <span className={`header__infos__status ${isOpenNow ? 'opened' : 'closed'}`}>
+                {isOpenNow ? 'Aberto!' : 'Fechado'}
+              </span>{' • '}
+              {configs.randomPlace}{' • '}
+              Entrega {configs.averageDeliveryDuration}
+            </p>
+          </div>
+          <div className="col">
+            <button className="matter-button-contained big">
+              <img src="/logo-whatsapp.svg"/>
+              <span>{configs.fakePhone}</span>
+            </button>
+          </div>
         </div>
-        <div className="col">
-          <button className="matter-button-contained big">
-            <img src="/logo-whatsapp.svg"/>
-            <span>{configs.fakePhone}</span>
-          </button>
-        </div>
-      </div>
-    </header>
+      </header>
+
+      <nav className="navbar" role="navigation">
+        <ul className="center">
+          <li><NavLink exact to="/" className="matter-button-outlined">Página inicial</NavLink></li>
+          <li><NavLink to="cardapio" className="matter-button-outlined">Cardápio</NavLink></li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
